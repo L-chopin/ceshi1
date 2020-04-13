d62591f330d4a9932cabe11d6ba043ca0fbe48a7
L-chopin <151508021@qq.com>
2020-04-14 06:35:00 +0800

    pycharm提交的内容

---
+++ demo.py
@@ -0,0 +1,10 @@
+print(1)
+
+# pycharm + git 相结合，需要的步骤
+# 1.配置git，让pycharm能使用git
+# 2.配置github账号（可选）
+# 3.初始化仓库，相当于git init
+# 4.配置忽略文件 .gitignore（可能需要安装插件）
+# 5.添加文件到暂存区，相当于git add
+# 6.添加文件到本地仓库，相当于git commit -m “xxx”
+# 7.推送到远程仓库，相当于git push xxx（远程仓库地址）
